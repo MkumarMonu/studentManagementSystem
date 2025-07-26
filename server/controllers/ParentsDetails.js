b0a9596ae4585ecb6e8a87091a8642eb1ec33af6
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-07-26 16:48:50 +0530

    get all user details

--- server/controllers/ParentsDetails.js
+++ server/controllers/ParentsDetails.js
@@ -100,9 +100,8 @@ exports.getAllUserDetails = async (req, res) => {
   try {
     const id = req.user.id;
     const userDetails = await User.findById(id)
-      .populate("additionalDetails")
+      .populate("parentsDetails")
       .exec();
-    console.log(userDetails);
     res.status(200).json({
       success: true,
       message: "User Data fetched successfully",
